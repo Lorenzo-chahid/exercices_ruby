e144eccd56be2d0cb224056a77a32d728b30477a
Lorenzo Chahid <lorenzo@listminut.com>
2021-10-04 15:51:22 +0200

    good like method need to converter to class

--- converter_number.rb
+++ converter_number.rb
@@ -3,14 +3,17 @@ class Converter
 
   def converter_to_roman(num)
     alpha_roman = {
-      1000 => "M",
-      900  => "CM", 500 => "D", 400 => "CD", 100 => "C",
-      90   => "XC", 50  => "L", 40  => "XL", 10  => "X",
-      9    => "IX", 5   => "V", 4   => "IV", 1   => "I"
+      1000 => 'M',
+      900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C',
+      90 => 'XC', 50  => 'L', 40  => 'XL', 10  => 'X',
+      9 => 'IX', 5   => 'V', 4   => 'IV', 1   => 'I'
     }
-    alpha_roman.reduce('') do |ac, digit|
+    alpha_roman.reduce('') do |ac, number|
+      key, value = number
+      occurence, num = num.divmod(key)
       puts "Here my accumulator #{ac}"
-      puts "here my digit #{digit}"
+      puts "here my number #{number}"
+      ac + value*occurence
     end
   end
 
@@ -22,6 +25,7 @@ d = Converter.new
 
 
 puts a.converter_to_roman(1000)
+puts b.converter_to_roman(789)
 #puts b.romanize(100)
 #puts c.romanize(22)
 #puts d.romanize(678)
