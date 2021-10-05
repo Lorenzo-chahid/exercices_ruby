c44784026d802b359cbd8c47e9cd3a4811e1031f
Lorenzo Chahid <lorenzo@listminut.com>
2021-10-05 11:54:46 +0200

    init converter to arabic

--- converter_number.rb
+++ converter_number.rb
@@ -1,29 +1,26 @@
 class Converter
 
-
-  def converter_to_roman(num)
-    alpha_roman = {
-      1000 => 'M',
-      900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C',
-      90 => 'XC', 50  => 'L', 40  => 'XL', 10  => 'X',
-      9 => 'IX', 5   => 'V', 4   => 'IV', 1   => 'I'
-    }
-    alpha_roman.reduce('') do |ac, number|
+  def converter_to_arabic(num)
+    alpha.reduce(0) do |ac, number|
+      # ac = 0
+      # number = [ 'X','10]
       key, value = number
-      occurence, num = num.divmod(key)
-      ac + value*occurence
+      # key = 'X'
+      # value = 10
+
     end
   end
 
+  private
+
+  def alpha
+    {
+      'X' => 10,
+      'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1
+    }
+  end
+
 end
-a = Converter.new
-b = Converter.new
-c = Converter.new
-d = Converter.new
-
-
-puts a.converter_to_roman(1000)
-puts b.converter_to_roman(789)
 
 
 
--- roman_numeral.rb
+++ roman_numeral.rb
@@ -1,9 +1,19 @@
 class Converter
   def converter_to_roman(num)
     alpha.reduce('') do |ac, number|
+      # ac = ''
+      # number = [10 ,'X']
       key, value = number
+      # key = 10
+      # value = 'X'
       occurence, num = num.divmod(key)
+      #  25.divmod(10)
+      #  [2, 5]
+      # occurence = 2
+      # num = 5
       ac + value * occurence
+      #  '' + 'X' * 2
+      # 'XX'
     end
   end
 
@@ -11,11 +21,7 @@ class Converter
 
   def alpha
     {
-      1000000 => 'M*',
-      500000 => 'D*',
-      100000 => 'C*',
-      50000 => 'L*',
-      10000 => 'X*', 5000 => 'V*', 1000 => 'M',
+      1000 => 'M',
       900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C',
       90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X',
       9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I'
@@ -29,8 +35,8 @@ d = Converter.new
 
 
 
-puts c.converter_to_roman(1000000)
-puts a.converter_to_roman(56789)
+puts c.converter_to_roman(25)
+puts a.converter_to_roman(256)
 
 
 
